bef9cce1f68121913adad17169edc619070e4028
kmc7468 <kmc7468@naver.com>
2017-04-25 23:37:16 +0900

    Add Linking Mode configuration

---
+++ Include/Luce/Configuration/LinkingMode.hh
@@ -0,0 +1,8 @@
+#ifndef LUCE_HEADER_CONFIGURATION_LINKINGMODE_HH
+#define LUCE_HEADER_CONFIGURATION_LINKINGMODE_HH
+
+#ifdef LUCE_CONFIG_LINKING_MODE
+#error The linking mode configuration is invalid.
+#endif
+
+#endif
