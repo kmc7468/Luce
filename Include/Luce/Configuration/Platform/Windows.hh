5cb01201a340f923aeb077db2c78e9d363498014
kmc7468 <kmc7468@naver.com>
2017-04-26 18:36:21 +0900

    Rename Win32 -> Windows

---
+++ Include/Luce/Configuration/Platform/Windows.hh
@@ -0,0 +1,11 @@
+#ifndef LUCE_HEADER_CONFIGURATION_PLATFORM_WINDOWS_HH
+#define LUCE_HEADER_CONFIGURATION_PLATFORM_WINDOWS_HH
+#include <Luce/Configuration/Platform-Value.hh>
+
+#ifdef LUCE_CONFIG_PLATFORM
+#error The platform configuration is invalid.
+#endif
+
+#define LUCE_CONFIG_PLATFORM LUCE_CONFIG_WINDOWS
+
+#endif
