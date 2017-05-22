cffd28da7ae61084bd37994b83c14d081735b60b
kmc7468 <kmc7468@naver.com>
2017-05-22 21:22:51 +0900

    Fix LUCE_MACRO_STATIC_ASSERT bugs

--- Source/Assertion-Common.cc
+++ Source/Assertion-Common.cc
@@ -2,8 +2,16 @@
 
 #include <climits>
 
+#if LUCE_MACRO_IS_YES(LUCE_CONFIG_STATICASSERT)
 LUCE_MACRO_STATIC_ASSERT((sizeof(void*) * CHAR_BIT) >= 16,
 						 "Supports only 16-bit and higher platforms.");
 
 LUCE_MACRO_STATIC_ASSERT(CHAR_BIT == 8,
 						 "Only platforms with 8 bits per byte are supported.");
+#else
+LUCE_MACRO_STATIC_ASSERT((sizeof(void*) * CHAR_BIT) >= 16,
+						 Supports_only_16_bit_and_higher_platforms);
+
+LUCE_MACRO_STATIC_ASSERT(CHAR_BIT == 8,
+						 Only_platforms_with_8_bits_per_byte_are_supported);
+#endif
