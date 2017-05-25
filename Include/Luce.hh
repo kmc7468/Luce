31a8e36024307f99631a9cf107087f827502e7d9
kmc7468 <kmc7468@naver.com>
2017-05-25 18:48:15 +0900

    Add Luce-AI

--- Include/Luce.hh
+++ Include/Luce.hh
@@ -1,6 +1,7 @@
 #ifndef LUCE_HEADER_LUCE_HH
 #define LUCE_HEADER_LUCE_HH
 
+#include <Luce/AI.hh>
 #include <Luce/Component.hh>
 #include <Luce/Exception.hh>
 #include <Luce/Memory.hh>
---
+++ Include/Luce/AI.hh
@@ -0,0 +1,4 @@
+#ifndef LUCE_HEADER_AI_HH
+#define LUCE_HEADER_AI_HH
+
+#endif
