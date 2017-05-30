029def1836ee35f4ae9a1d103487f40a31d5e644
kmc7468 <kmc7468@naver.com>
2017-05-30 21:58:57 +0900

    Update Luce-AI

---
+++ Include/Luce/AI/Configuration.hh
@@ -0,0 +1,6 @@
+#ifndef LUCE_HEADER_AI_CONFIGURATION_HH
+#define LUCE_HEADER_AI_CONFIGURATION_HH
+
+#include <Luce/AI/Configuration/ComputeByCpu.hh>
+
+#endif
--- Include/Luce/AI/Layer.hh
+++ Include/Luce/AI/Layer.hh
@@ -2,14 +2,12 @@
 #define LUCE_HEADER_AI_LAYER_HH
 #include <Luce/Configuration.hh>
 
-#include <Luce/AI/AIObject.hh>
-
 namespace Luce
 {
 	namespace AI
 	{
 		template<typename Elem_>
-		class Layer : public AIObject
+		class Layer
 		{
 		public:
 			virtual ~Layer();
