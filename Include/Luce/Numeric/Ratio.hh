7f7688fbd69bc7c351ea4b7af4e3fed1124e1526
kmc7468 <kmc7468@naver.com>
2017-06-03 23:04:48 +0900

    Add Ratio typedefs

--- Include/Luce/Numeric/Ratio.hh
+++ Include/Luce/Numeric/Ratio.hh
@@ -31,6 +31,24 @@ namespace Luce
 #endif
 				;
 		};
+
+		typedef Ratio<1000000000000000000, 1> Exa;
+		typedef Ratio<1000000000000000, 1> Peta;
+		typedef Ratio<1000000000000, 1> Tera;
+		typedef Ratio<1000000000, 1> Giga;
+		typedef Ratio<1000000, 1> Mega;
+		typedef Ratio<1000, 1> Kilo;
+		typedef Ratio<100, 1> Hecto;
+		typedef Ratio<10, 1> Deca;
+
+		typedef Ratio<1, 1000000000000000000> Atto;
+		typedef Ratio<1, 1000000000000000> Femto;
+		typedef Ratio<1, 1000000000000> Pico;
+		typedef Ratio<1, 1000000000> Nano;
+		typedef Ratio<1, 1000000> Micro;
+		typedef Ratio<1, 1000> Milli;
+		typedef Ratio<1, 100> Centi;
+		typedef Ratio<1, 10> Deci;
 	}
 
 	using Luce::Numberic::Ratio;
--- Test-Projects/Test1/main.cpp
+++ Test-Projects/Test1/main.cpp
@@ -5,12 +5,15 @@ using namespace Luce;
 #include <cstdio>
 #include <iostream>
 #include <thread>
+#include <ratio>
 
 int main()
 {
 	typedef Numberic::Ratio<1, 3> v;
 
 	std::printf("%f", v::Real);
+
+	
 
 	return 0;
 }
