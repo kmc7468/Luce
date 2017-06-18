f99cb1e1df4143282bda2ee3b7e8c550fccdecce
kmc7468 <kmc7468@naver.com>
2017-06-18 13:04:04 +0900

    Update InsertionSort

--- Include/Luce/Algorithm/Detail/InsertionSort.hh
+++ Include/Luce/Algorithm/Detail/InsertionSort.hh
@@ -11,7 +11,21 @@ namespace Luce
 		template<typename Ty_>
 		void InsertionSort(std::vector<Ty_>& data)
 		{
-			
+			for (std::size_t i = 1; i < data.size(); ++i)
+			{
+				std::size_t j = i - 1;
+				Ty_ temp = data[i];
+
+				while (j >= 0 && temp < data[j])
+				{
+					std::swap(data[j], data[j + 1]);
+					if (j == 0)
+					{
+						break;
+					}
+					--j;
+				}
+			}
 		}
 		template<typename Ty_, std::size_t Len_>
 		void InsertionSort(Ty_(&data)[Len_])
