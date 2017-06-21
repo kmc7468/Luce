6f71407f250dbc0428ceba33aeef345cc601db34
kmc7468 <kmc7468@naver.com>
2017-06-21 20:54:30 +0900

    Update Luce-Utility

--- Include/Luce/Utility.hh
+++ Include/Luce/Utility.hh
@@ -1,6 +1,7 @@
 #ifndef LUCE_HEADER_UTILITY_HH
 #define LUCE_HEADER_UTILITY_HH
 
+#include <Luce/Utility/AlwaysInline.hh>
 #include <Luce/Utility/Blank.hh>
 #include <Luce/Utility/Integer.hh>
 #include <Luce/Utility/LabledLoop.hh>
